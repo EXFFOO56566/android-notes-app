b700efbbaac470c13496c0112e83a675e872c7bc
tochyodikwa <odikwatochy233@gmail.com>
2023-04-11 00:20:04 -0700

    First

---
+++ app/src/main/java/nemosofts/notes/app/Constant/Constant.java
@@ -0,0 +1,25 @@
+package nemosofts.notes.app.Constant;
+
+import java.io.Serializable;
+
+public class Constant implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public static String company = "Nemosofts";
+    public static String email = "info.nemosofts@gmail.com";
+    public static String website = "https://www.nemosoftscom/";
+    public static String contact = "+00 020456089";
+    public static String privacyPolicy = "http://example.com/";
+
+    //--------------------Admob ads----------------- //
+    public static Boolean isAdmobBannerAd = true;
+    public static Boolean  isAdmobInterAd = true;
+    public static int adShow = 5;
+
+    //--------------------Fb ads----------------- //
+    public static Boolean isFBBannerAd = false;
+    public static Boolean  isFBInterAd = false;
+    public static int adShowFB = 5;
+
+
+}
